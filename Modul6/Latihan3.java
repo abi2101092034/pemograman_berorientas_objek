f768007486aa9986bcbfaf084b227dea6e20f813
abi2101092034 <abilutfiandyy@gmail.com>
2022-11-16 11:30:18 +0700

    latihan modul 6
    Abi Lutfiandy Hendri
    2101092034

---
+++ Modul6/Latihan3.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul6;
+
+/**
+ *
+ * @author user
+ */
+
+import java.io.*;
+public class Latihan3 {
+    public static void main(String[] args) {
+        BufferedReader dataIn = new BufferedReader (new InputStreamReader (System.in));
+        
+        String nama = "";
+        
+        System.out.println("==Program Menginput Nama==\n");
+        
+        try {
+            System.out.print("Masukkan Nama Anda : ");
+            nama = dataIn.readLine();
+        }
+        catch (IOException e){
+            System.out.println("Error!");
+        }
+        
+        System.out.println("\n");
+        
+        int x = 1;
+        do{
+            System.out.println(nama);
+            x++;
+        } while(x<=100);
+    }
+}
